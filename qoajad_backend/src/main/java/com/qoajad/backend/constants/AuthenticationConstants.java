f102f90c15068c4f24f98232dc7d655cbece8b4a
Juan Sebastian Quiceno <juan.2114@hotmail.com>
2020-04-27 16:29:14 -0500

    New package for constants was added.

---
+++ qoajad_backend/src/main/java/com/qoajad/backend/constants/AuthenticationConstants.java
@@ -0,0 +1,7 @@
+package com.qoajad.backend.constants;
+
+public class AuthenticationConstants {
+
+    public static final String AUTHENTICATION_COOKIE_NAME = "authentication";
+    public static final String AUTHENTICATION_ROOT_URI = "authentication/";
+}
